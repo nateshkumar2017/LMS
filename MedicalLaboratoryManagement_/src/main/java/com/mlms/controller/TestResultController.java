88717886ca34880dc2de763b012d13c959cdc879
Ebadd <ebadmubashir@gmail.com>
2023-10-02 18:38:45 +0500

    After updating result Report status set to Available

--- MedicalLaboratoryManagement_/src/main/java/com/mlms/controller/TestResultController.java
+++ MedicalLaboratoryManagement_/src/main/java/com/mlms/controller/TestResultController.java
@@ -2,7 +2,11 @@ package com.mlms.controller;
 
 import com.mlms.dtos.TestAttributeResultDTO;
 import com.mlms.dtos.TestResultDTO;
+import com.mlms.entities.Order;
+import com.mlms.entities.ReportStatus;
 import com.mlms.entities.TestResult;
+import com.mlms.repo.OrderRepo;
+import com.mlms.repo.ReportStatusRepo;
 import com.mlms.service.TestResultServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +23,12 @@ public class TestResultController {
     @Autowired
     private TestResultServiceImpl testResultService;
 
+    @Autowired
+    private OrderRepo orderRepository;
+
+    @Autowired
+    private ReportStatusRepo reportStatusRepository;
+
 
 
 //    @PostMapping("/create")
@@ -30,12 +40,32 @@ public class TestResultController {
 //        return testResultService.createTestResult(orderId, testId, attributeResults);
 //    }
 
+//    @PostMapping("/create")
+//    public List<TestResult> createTestResults(@RequestBody TestResultDTO testResultDTO) {
+//        Long orderId = testResultDTO.getOrderId();
+//        Long testId = testResultDTO.getTestId();
+//        List<TestAttributeResultDTO> attributeResults = testResultDTO.getAttributesResults();
+//        return testResultService.createTestResults(orderId, testId, attributeResults);
+//    }
+
     @PostMapping("/create")
     public List<TestResult> createTestResults(@RequestBody TestResultDTO testResultDTO) {
         Long orderId = testResultDTO.getOrderId();
         Long testId = testResultDTO.getTestId();
         List<TestAttributeResultDTO> attributeResults = testResultDTO.getAttributesResults();
-        return testResultService.createTestResults(orderId, testId, attributeResults);
+
+
+        List<TestResult> testResults = testResultService.createTestResults(orderId, testId, attributeResults);
+
+        Order orderToUpdate = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found with ID: " + orderId));
+
+        ReportStatus availableStatus = reportStatusRepository.findByStatus(ReportStatus.StatusType.AVAILABLE);
+
+        orderRepository.updateOrderStatus(orderId, availableStatus);
+
+
+        return testResults;
     }
 
 }
--- MedicalLaboratoryManagement_/src/main/java/com/mlms/repo/OrderRepo.java
+++ MedicalLaboratoryManagement_/src/main/java/com/mlms/repo/OrderRepo.java
@@ -3,16 +3,27 @@ package com.mlms.repo;
 import com.mlms.entities.Order;
 import com.mlms.entities.ReportStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface OrderRepo extends JpaRepository<Order,String> {
     //List<Order> findByStatusId(int statusId);
     List<Order> findByReportStatus(ReportStatus reportStatus);
 
   //  Optional<Object> findById(Long orderId);
     Optional<Order> findById(Long testId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Order o SET o.reportStatus = ?2 WHERE o.id = ?1")
+    void updateOrderStatus(Long orderId, ReportStatus status);
+
 
     //Optional<Order> findById(Long id);
 }
